5c1881076ad533e320ea590ad0d347538a5f96dd
maiha <maiha@wota.jp>
2023-01-27 10:22:44 +0900

    %表示対応

--- docs/main.js
+++ docs/main.js
@@ -1,18 +1,107 @@
+// 特殊効果のレート情報
+class EffectRatio {
+  name; // "固定詠唱"
+  val;  // 50 
+  max;  // 100
+  unit; // "%"
+  pct;  // 50 
+
+  static default_max = 100
+
+  constructor(name, val, max, unit) {
+    this.name = name
+    this.val  = val
+    this.max  = max || EffectRatio.default_max
+    this.unit = unit
+    this.pct  = Math.min(100, Math.trunc(this.val * 100 / this.max))
+  }
+
+  serialize = function() {
+    return EffectRatio.serialize(this)
+  }
+
+  pretty_pct = function() {
+    return EffectRatio.pretty_pct(this)
+  }
+
+  val_with_unit = function() {
+    return `${this.val}${this.unit}`
+  }
+
+  static regexp_source  = '^%(\\d+)(/(\\d+))?(.?)\\[([^\\]\n]*?)\\]'
+  static scan_regexp    = new RegExp(EffectRatio.regexp_source, 'gmu')
+  static unapply_regexp = new RegExp(EffectRatio.regexp_source, 'u')
+
+  /*
+  ```js
+  "%50[固定詠唱]と\n%100[石化耐性]が欲しい".match(/^%(\d+)\[([^\]\n]*?)\]/gmu)
+  // => (2) ['%50[固定詠唱]', '%100[石化耐性]']
+  ```
+  */
+  static scan = function(text) {
+    const ratios = []
+    const match = text.match(EffectRatio.scan_regexp)
+
+    match?.forEach(full => { // "%50[固定詠唱]"
+      const ratio = EffectRatio.deserialize(full)
+      if (ratio) {
+        console.debug(ratio)
+        ratios.push(ratio)
+      }
+    })
+
+    return ratios
+  }
+
+  static pretty_pct = function(v) {
+    let den = ''
+    if (v.max != EffectRatio.default_max) den += v.max.toString()
+    if (den.length > 0) den = `/${den}`
+    return `${v.val}${den}${v.unit}`
+  }
+
+  static serialize = function(v) {
+    return `%${EffectRatio.pretty_pct(v)}[${v.name}]`
+  }
+
+  static deserialize = function(v) {
+    const md = v.match(EffectRatio.unapply_regexp)
+    if (md) {
+      console.log(md)
+      // ['%50/80%[固定詠唱]', '50', '/80', '80', '%', '固定詠唱', index: 0, input: '%50/80%[固定詠唱]'
+      // ['%20/30[全ステ+]', '20', '/30', '30', '', '全ステ+', index: 0, input: '%20/30[全ステ+]'
+      // ['%79%[ディレイ]', '79', undefined, undefined, '%', 'ディレイ', index: 0, input: '%79%[ディレイ]'
+      const name  = md[5] // '固定詠唱'
+      const val   = Number(md[1]) // 50
+      const max   = Number(md[3]) || 100 // 80
+      const unit  = md[4] || ''  // '%'
+      const ratio = new EffectRatio(name, val, max, unit)
+      console.log(ratio)
+      return ratio
+    } else {
+      return null
+    }
+  }
+}
+
 class Inventory {
   static CSS = {
     data: 'ROEQP-data',
     invt: 'ROEQP-invt',
     view: 'ROEQP-invt-view',
+    bars: 'ROEQP-invt-bars',
     form: 'ROEQP-invt-form',
     grid: 'ROEQP-invt-view-grid',
   }
 
   invt;
   view;
+  bars;
   form;
   grids;
   textarea;
   items = [];
+  ratios = [];
   comments = [];
   opts = {
     normalize_source: true,
@@ -25,6 +114,7 @@ class Inventory {
 
     this.invt  = this.construct_invt(root)
     this.view  = this.construct_view()
+    this.bars  = this.construct_bars()
     this.form  = this.construct_form()
     this.grids = [...Array(10).keys()].map(pos => this.construct_grid(pos))
   }
@@ -34,14 +124,22 @@ class Inventory {
     if (opts.source) this.opts.source = opts.source
     if (this.opts.source) this.textarea?.val(this.opts.source)
     const source = this.textarea?.val() || ''
+
+    this.ratios = EffectRatio.scan(source)
     this.items = Roeqp.scan(source)
     this.comments = source.match(/^\s*(# .*?)$/mg)?.map(i => i.trim()) || []
     this.render()
   }
 
   render() {
+    this.render_bars(this.ratios)
+
     if (this.opts.normalize_source) {
       let text = ''
+      if (this.ratios.length > 0) {
+        this.ratios.forEach(ratio => text += `${ratio.serialize()}\n`)
+        text += "\n"
+      }
       this.items.forEach(item => text += `${item.label()}\n`)
       text += "\n"
       this.comments.forEach(comment => text += `${comment}\n`)
@@ -78,6 +176,31 @@ class Inventory {
     });
   }
 
+  render_bars(ratios) {
+    const table = $('<table>', {class: "bar"})
+    console.debug(ratios)
+    $.each(ratios, (idx, ratio) => {
+      const rank = Math.trunc(ratio.pct / 25.0) * 25
+    
+      // <div class="name">固定詠唱</div>
+      const label = $('<div>', {class: "name", text: ratio.name})
+      // <progress value="70" max="100"></progress><span>70 %</span>
+      //const progress = $('<progress>', {class: `progress rank-${rank}`, value: ratio.pct, max: ratio.max})
+      const progress = $('<div>', {class: `progress rank-${rank}`})
+      progress.append($('<div>', {class: 'bar', width: `${ratio.pct}%`}))
+      const percent  = $('<span>', {class: "percent", text: ratio.val_with_unit()})
+
+      const tr = $('<tr>', {class: `rank-${rank}`})
+      tr.append($('<td>', {class: "label"}).append(label))
+      tr.append($('<td>', {class: "progress"}).append(progress))
+      tr.append($('<td>', {class: "percent"}).append(percent))
+      table.append(tr)
+    })
+
+    this.bars.empty()
+    this.bars.append(table)
+  }
+
   render_grid(pos, item) {
     const grid = this.grids[pos]
     if (!grid) {
@@ -97,6 +220,24 @@ class Inventory {
     const css = Inventory.CSS.invt
     root.addClass(`invt ${css}`)
     return root
+  }
+
+  // root -> bars
+  construct_bars() {
+    const css = Inventory.CSS.bars
+    const el = this.invt.find(`.${css}`)
+    if (el.length > 0) {
+      return el
+    } else {
+      /*
+        <div class="ROEQP-invt-bars bars">
+        </div>
+      */
+      const div = $('<div>', {class: `${css} bars`})
+      this.invt.append(div)
+      console.debug(`ROEQP: [#${this.invt.attr("id")}.${css}] is not found. generated.`)
+      return div
+    }
   }
 
   // root -> form
@@ -218,10 +359,11 @@ ROEQP.Form = {
 
 ROEQP.change_form = function(value) {
   const css = Inventory.CSS
+  const key = `.${css.bars}, .${css.form}`
   switch (value) {
-    case ROEQP.Form.Right:  $(`.${css.form}`).css("float", "left").show(); break
-    case ROEQP.Form.Bottom: $(`.${css.form}`).css("float", "none").show(); break
-    case ROEQP.Form.Hide:   $(`.${css.form}`).hide(); break
+    case ROEQP.Form.Right:  $(key).css("float", "left").show(); break
+    case ROEQP.Form.Bottom: $(key).css("float", "none").show(); break
+    case ROEQP.Form.Hide:   $(key).hide(); break
   }
 };
 
@@ -250,7 +392,7 @@ ROEQP.set_frame = function(value) {
     case ROEQP.Frame.All:  box = true; item = true; break
     case ROEQP.Frame.None: break
   }
-  console.log("set_frame", value, box, item)
+  console.debug("set_frame", value, box, item)
   $(`.${css.view}`).css("border-width", box ? "1px" : "0")
   $(`.${css.view} .grid`).css("border-width", item ? "1px" : "0")
 };
