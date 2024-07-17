04b941122188850ada4ca19aa0f434933ccd5ace
maiha <maiha@wota.jp>
2024-07-18 07:03:27 +0900

    画像の参照先をDivine Prideに変更

--- docs/main.js
+++ docs/main.js
@@ -275,7 +275,7 @@ class Inventory {
     grid.empty()
 
     if (item) {
-      grid.append($('<img>', {class: "icon", src: `https://rotool.gungho.jp/images/item/${item.id}.png`}))
+      grid.append($('<img>', {class: "icon", src: `https://static.divine-pride.net/images/items/collection/${item.id}.png`}))
       grid.append($('<div>', {class: "name", text: item.label()}))
     }
   }
